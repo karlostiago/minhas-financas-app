43a44d916bf56e128e0f8694b5b835af46edf98c
Carlos Tiago <carlostiagodesousa@gmail.com>
2020-11-22 20:05:54 -0300

    atualizando rota default

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -29,7 +29,7 @@ function Rotas(props) {
     return (
         <HashRouter>
             <Switch>
-                <Route path="/" component={Login} />
+                {/* <Route path="/" component={Login} /> */}
                 <Route path="/login" component={Login} />
                 <Route path="/cadastro-usuario" component={CadastroUsuario} />
 
