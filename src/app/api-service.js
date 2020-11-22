a66fe6bb47db2c470b8f5d8e44063e124cee601b
Carlos Tiago <carlostiagodesousa@gmail.com>
2020-11-22 20:02:22 -0300

    atualizando url de acesso a api

--- src/app/api-service.js
+++ src/app/api-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8081'
+    baseURL: 'https://minhasfinancas-application-api.herokuapp.com/'
 });
 
 class ApiService {
